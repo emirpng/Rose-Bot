74228ddd2d7f4c166e802bc8d4f49fa25929b696
Emir <marveldarkbull@gmail.com>
2022-11-27 23:25:04 +0300

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,7 +18,7 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-djndn
+Merhaba {}, Benim adım {}!
 """
 
 HELP_STRINGS = """
