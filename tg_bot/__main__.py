2e6e123a5514c3314f2f4ff3a531bb1d7fc2fe75
Emir <marveldarkbull@gmail.com>
2022-11-27 23:32:48 +0300

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,7 +18,8 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-**Merhaba {}, Benim adım {}! Ben bir Roll Roys Federasyonuna ait dijital grup asistanıyım**
+**Merhaba {}, Benim adım {}! 
+Ben bir Roll Roys Federasyonuna ait dijital grup asistanıyım**
 """
 
 HELP_STRINGS = """
